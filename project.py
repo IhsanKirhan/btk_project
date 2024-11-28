51e4326c1586900ec5a66950210d9eff7fa12f0f
ihsan kirhan <78253799+IhsanKirhan@users.noreply.github.com>
2024-11-28 15:19:16 +0300

    Update project.py

--- project.py
+++ project.py
@@ -2,28 +2,19 @@ import streamlit as st
 from PIL import Image
 from ultralytics import YOLO
 
-st.title("YOLO Object Detection Application")
+
+model = YOLO("best_model.pt")
 
 
-
-@st.cache_resource
-def load_model(model_name):
-    return YOLO(model_name)
-
-
-model = load_model(model_files["best_model.pt"])
-
-
-uploaded_file = st.file_uploader("Upload a PNG file", type=["png"])
+uploaded_file = st.file_uploader("Upload a face photo (PNG)", type=["png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Image', use_column_width=True)
     
 
-    st.write("### Performing Object Detection...")
+    st.write("Emotion detection...")
     results = model.predict(image)
-    
-  
+
     result_image = results[0].plot()  
-    st.image(result_image, caption='Detection Result', use_column_width=True)
+    st.image(result_image, caption='Result:', use_column_width=True)
